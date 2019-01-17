528abd7bf182bd38463c77f2a8c68af32f18c384
Juwel <amanatjuwel@gmail.com>
2019-01-17 12:20:13 +0600

    array.php added

---
+++ array.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+  
+
+$data = array(
+        'name' => "Name goes here",
+    );
+echo $data['name'];
+?>	
+</body>
+</html>
